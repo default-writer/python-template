ddc8860637b75ad22b6d8f75aca0e96539cee1e2
default-writer <funcelot@devweb.app>
2022-11-30 17:57:38 +0000

    updated venv scripts

--- bin/venv.sh
+++ bin/venv.sh
@@ -1,6 +1,13 @@
 #!/bin/bash -e
-python3 -m venv venv
-./venv/bin/activate
+pwd=$(pwd)
+
+cd "${0%/*}"
+${pwd}/bin/clean.sh
+cd ${pwd}
+
+rm -rf ${pwd}/venv
+
+python3 -m venv ${pwd}/venv
+chmod +x ${pwd}/venv/bin/activate && ${pwd}/venv/bin/activate
 python3 -m pip install --upgrade pip
 python3 -m pip install --user -r requirements.txt
-
