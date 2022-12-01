9af12f4e73c91bc1293d7ce401183d2b3e9abed2
default-writer <funcelot@devweb.app>
2022-12-01 02:09:16 +0000

    code cleanup

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -e
+pwd=$(pwd)
+
 sudo git config --global --add safe.directory ${pwd}
 sudo apt install -y build-essential curl git ca-certificates python3 python3-dev python3-pip python3-venv python3-behave
+
+cd ${pwd}
