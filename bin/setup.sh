b5b96ccb8f82a749684b5158d58f2f49240ad27c
defaut-writer <funcelot@outlook.com>
2022-12-01 09:05:22 +0300

    updated setup scripts

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,35 +1,42 @@
 #!/bin/bash -e
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
+uid=$(id -u)
+
+if [ ! "${uid}" -eq 0 ]
+then
+  echo "Please run as root"
   exit
 fi
 
 pwd=$(pwd)
 
-sudo git config --global --add safe.directory ${pwd}
+# sudo git config --global --add safe.directory ${pwd}
 
-sudo apt-get update
-sudo apt install -y build-essential curl git ca-certificates python3 python3-dev python3-pip python3-venv python3-behave
+# sudo apt-get update
+# sudo apt install -y build-essential curl git ca-certificates python3 python3-dev python3-pip python3-venv python3-behave
 
-if [ "$1"=="docker" ]
-then 
-    sudo apt-get update
-    sudo apt-get install -y \
-        ca-certificates \
-        curl \
-        gnupg \
-        lsb-release
-    sudo mkdir -p /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+install="$1"
 
-    sudo apt-get update
-    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+if [ "${install}" == "docker" ]
+then
+  echo docker
+  exit
+  sudo apt-get update
+  sudo apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+  sudo mkdir -p /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-    sudo apt-get update
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+  sudo apt-get update
+  sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+  sudo apt-get update
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 fi
 
 cd ${pwd}
--- bin/venv.sh
+++ bin/venv.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -e
-if [ "$EUID" -e 0 ]
-  then echo "Please run as user"
-  exit
+uid=$(id -u)
+
+if [ -z "${uid}" ]
+then
+    echo "Please run as user"
+    exit
 fi
 
 pwd=$(pwd)
@@ -13,6 +16,7 @@ cd ${pwd}
 rm -rf ${pwd}/venv
 
 python3 -m venv ${pwd}/venv
+
 chmod +x ${pwd}/venv/bin/activate && ${pwd}/venv/bin/activate
 python3 -m pip install --require-virtualenv --upgrade pip
 python3 -m pip install --require-virtualenv -r requirements.txt
