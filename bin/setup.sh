038dd6535b3b6c41b52cc03966b089faf7c213de
default-writer <funcelot@outlook.com>
2022-12-01 10:08:21 +0300

    updated setup docker

--- bin/setup.sh
+++ bin/setup.sh
@@ -33,11 +33,11 @@ then
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-  sudo apt-get update
+  sudo apt-get update -y
   sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-  sudo apt-get update
-  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+  sudo apt-get update -y
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 fi
 
 cd ${pwd}
