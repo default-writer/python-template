6790d36e1120b6546fcde14d57b33fe98054c3f6
defaut-writer <funcelot@outlook.com>
2022-12-01 07:57:42 +0300

    added setup for docker in .devcontainer

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,6 +2,29 @@
 pwd=$(pwd)
 
 sudo git config --global --add safe.directory ${pwd}
+
+sudo apt-get update
 sudo apt install -y build-essential curl git ca-certificates python3 python3-dev python3-pip python3-venv python3-behave
+
+if [ "$1"=="docker" ]
+then 
+    sudo apt-get update
+    sudo apt-get install -y \
+        ca-certificates \
+        curl \
+        gnupg \
+        lsb-release
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    sudo apt-get update
+    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+    sudo apt-get update
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+fi
 
 cd ${pwd}
